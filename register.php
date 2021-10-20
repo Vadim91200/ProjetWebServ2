9947ec1b308d0c56072583e5f23a11e3558210dd
Alexis <leonalex@free.fr>
2021-10-20 15:44:44 +0200

    Factorisation header

--- register.php
+++ register.php
@@ -40,34 +40,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <body>
     <div class="main-sec inner-page">
         <!-- //header -->
-        <header class="py-sm-3 pt-3 pb-2" id="home">
-            <div class="container">
-                <!-- nav -->
-                <div class="top-w3pvt d-flex">
-                    <div id="logo">
-                        <h1> <a href="index.html"><span class="log-w3pvt">B</span>aggage</a> <label class="sub-des">Online Store</label></h1>
-                    </div>
-
-                    <div class="forms ml-auto">
-                        <a href="login.html" class="btn"><span class="fa fa-user-circle-o"></span> Sign In</a>
-                        <a href="register.html" class="btn"><span class="fa fa-pencil-square-o"></span> Sign Up</a>
-                    </div>
-                </div>
-                <div class="nav-top-wthree">
-                    <nav>
-                        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
-                        <input type="checkbox" id="drop" />
-                        <ul class="menu">
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="shop.html">Catalogue</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </nav>
-                    <!-- //nav -->
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </header>
+        <?php include 'header.php'; ?>
         <!-- //header -->
 
     </div>
