d7acf83d53b1c50f2cd837849308a6f3c2daebad
Vadim91200 <62403530+Vadim91200@users.noreply.github.com>
2021-12-18 12:00:51 +0100

    Final Code refactoring

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+function render($view) {
+	global $controller;
+        require('views/header.php');
+	require('views/'.$view.'.php');
+    echo '<!--';
+    print_r($_SESSION);
+    echo '-->';
+}
+
+function e($s) {
+    return ($s);
+}
+
+if(!isset($_GET['controller'])) {
+	$_GET['controller'] = "home";
+}
+$controller = $_GET['controller'];
+
+include('configbd.php');
+
+require('controllers/'.$controller.'.php');
