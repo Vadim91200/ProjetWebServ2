dc23a9e6bcba22cc5642e9e9e586f1e55fdecc8b
Maxime Wang <888maximewang@gmail.com>
2021-12-19 01:11:31 +0100

    #comment Modify the path for the connection to the db

--- configbd.php
+++ configbd.php
@@ -1,6 +1,6 @@
 <?php
 $role = true;
-$dsn = "sqlite:database.sqlite";
+$dsn = "sqlite:database.db";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
